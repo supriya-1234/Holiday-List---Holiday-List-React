c40b35627eec25df8dc95e4b030ea33858ed2756
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-21 07:43:17 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    let cities = [
+      "Goa(India)",
+      "Amsterdam(Netherlands)",
+      "New York(USA)",
+      "Darjeeling(India)",
+      "Tokyo(Japan)",
+      "Lonavala(India)"
+    ];
+    return (
+      <>
+        <ol>
+          {this.cities.map((city, index) => (
+            <li key={"location" + (index + 1)}>{city}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
